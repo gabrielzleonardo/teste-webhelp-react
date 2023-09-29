bb14eb3a7b13db3f7618a1a8a04aeecad68139bf
gabriel <gleonardoalano@gmail.com>
2023-09-29 18:14:39 -0300

    feat: logo component

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,11 @@
+import logo from "/images/webhelp.svg";
+const Logo = () => {
+  return (
+    <div className="logo-wrapper">
+      <img src={logo} alt="Webhelp logo" />
+      <span>pay</span>
+    </div>
+  );
+};
+
+export default Logo;
